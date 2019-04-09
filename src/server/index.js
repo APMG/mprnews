6d23b49e49b71752fd1641cf8f28cad93f643522
Jason Phan <jphan@mpr.org>
2019-04-08 20:17:46 -0500

    commit before addingapollo

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import React from 'react';
+import { ServerLocation } from 'apm-titan';
 import { renderToString } from 'react-dom/server';
-// import { StaticRouter } from 'react-router-dom';
+// import { ApolloProvider, renderToStringWithData } from 'react-apollo';
+// import { HelmetProvider } from 'react-helmet-async';
 import App from '../shared/App';
 
 const app = express();
@@ -10,19 +12,28 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
+  const context = {};
+  const markup = renderToString(
+    <ServerLocation url={req.url} context={context}>
+      <App />
+      {req.url}
+    </ServerLocation>
+  );
+
   res.send(`
     <!DOCTYPE html>
     <html>
       <head>
         <title>MPR NEWS WITH SSR</title>
-        <script src="/bundle.js" defer>${<App />}</script>
+        <script src="/bundle.js" defer></script>
       </head>
 
       <body>
-        <div id="root"></div>
+        <div id="root">${markup}</div>
       </body>
     </html>
   `);
+  console.log(res);
 });
 
 app.listen(PORT, () => {
