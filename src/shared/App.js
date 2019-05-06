5459f375b95e7f4e8808303afa65a90dec1e3cdc
Jason Phan <jphan@mpr.org>
2019-05-06 16:29:02 -0500

    fixed component name

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Routes from '../shared/routes/routes';
 import '../shared/styles/index.scss';
-import RestApiHooksComponent from './components/Weather/WeatherHeader';
+import WeatherHeader from './components/Weather/WeatherHeader';
 
 class App extends Component {
   render() {
     return (
       <div>
         <img src="//mpr.apmcdn.org/news/1550179261168/img/mprnews.svg" alt="" />
-        <RestApiHooksComponent />
+        <WeatherHeader />
         <Routes />
       </div>
     );
