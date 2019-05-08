3fd3d593aff7ec3c13e565544af039f0c35e9624
Jason Phan <jphan@mpr.org>
2019-05-08 13:02:39 -0500

    output data to teaser component on collection page, I also removed some code that may not be used in the new site of MPR

--- src/shared/components/Collection/CollectionQuery.js
+++ src/shared/components/Collection/CollectionQuery.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 
-export function collectionQuery(contextSlug, pageNum) {
+export function collectionQuery(contextSlug, pageNum, topicSlug) {
   return gql`
     {
       collectionList: collectionList(contentAreaSlug: "${contextSlug}") {
@@ -18,11 +18,59 @@ export function collectionQuery(contextSlug, pageNum) {
             title
             canonicalSlug
           }
-       
+          totalItems
+          nextPage
+        }
+      }
+      collection: collection(contentAreaSlug: "news-taxonomy-test", slug: "topic/${topicSlug}") { 
+        id
+        title
+        results(page: ${pageNum}, pageSize: 18) {
+          nextPage
+          pageSize
+          totalPages
+          totalItems
+          previousPage
+          currentPage
+          items {
+            id
+            title
+            publishDate
+            description
+            canonicalSlug
+            primaryVisuals {
+              thumbnail {
+                aspect_ratios: aspectRatios {
+                  widescreen {
+                    instances {
+                      width
+                      height
+                      url
+                    }
+                  }
+                }
+                contentArea
+                dateTaken
+                dateline
+                fallback
+                longCaption
+                shortCaption
+                xid
+              }
+            }
+            audio {
+              title
+              durationHms
+              encodings {
+                httpFilePath
+              }
+            }
+          }
           totalItems
           nextPage
         }
       }
     }
+
   `;
 }
--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -1,30 +1,13 @@
 
 export const collectionConfig = {
   newsCoverage: [
-    {name: 'Arts', path: "/arts"}, 
-    {name: 'Books', path: "/thread"},
-    {name: 'Business', path: "/business"},
-    {name: 'Education', path: "/education"},
-    {name: 'Environment', path: "/environment"},
-    {name: 'Health', path: "/health"},
-    {name: 'Lifestyle', path: "/lifestyle"},
-    {name: 'Politics', path: "/politics"},
-    {name: 'Twin Cities', path: "/twin-cities"},
-   ],
-   newsPrograms: [
-    {name: 'Morning Edition', path: "/topic/morning-edition"}, 
-    {name: 'All things Considered', path: "/topic/all-things-considered"},
-    {name: 'MPR News with Kerri Miller', path: "/topic/kerri-miller"},
-    {name: 'MPR News with Angela Davis', path: "/topic/angela-davis"},
-    {name: 'MPR News Presents', path: "/topic/mpr-news-presents"},
-    {name: 'More programs', path: "/schedule"},
-   ],
-   newsResources: [
-    {name: 'Program schedule', path: "schedule"}, 
-    {name: 'Station directory', path: "https://www.mpr.org/listen/stations"},
-    {name: 'Podcasts', path: "/podcasts"},
-    {name: 'Archive', path: "https://archive.mpr.org/"},
-   ]
-
-
+    {name: 'Arts', path: "/topic/arts-and-culture"}, 
+    {name: 'Books', path: "/topic/arts-and-culture/books"},
+    {name: 'Business', path: "/topic/disaster/business-and-economics"},
+    {name: 'Education', path: "/topic/education"},
+    {name: 'Environment', path: "/topic/environment"},
+    {name: 'Health', path: "/topic/health"},
+    {name: 'Lifestyle', path: "/topic/arts-and-culture/lifestyle"},
+    {name: 'Politics', path: "/topic/politics-and-government"}  
+  ]
 }
