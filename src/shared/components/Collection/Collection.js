8cb52ec537c75d9d7d356737f8b9812f6ec15304
Kim Thompson <kthompson@mpr.org>
2019-05-16 14:40:16 -0500

    basic markup changes & npm updates

--- src/shared/components/Collection/Collection.js
+++ src/shared/components/Collection/Collection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Loading, Teaser } from 'apm-titan';
+import { Loading, Teaser, Heading } from 'apm-titan';
 import { truncateAmat } from '../../utils/utils';
 import { Body } from 'amat-react';
 import fallback from '../../assets/fallback.png';
@@ -12,40 +12,42 @@ const Collection = (props) => {
   if (!data || data.loading) return <Loading />;
   if (data.error) return <div>Error</div>;
   return (
-    <div>
-      <h1>{data.collection.title}</h1>
+    <section className="collection section">
+      <Heading level={2}>{data.collection.title}</Heading>
 
       {data.collection.results.items.map((collection) => {
         return (
           <Teaser
-            key={collection.id}
-            id={collection.id}
-            title={collection.title}
-            href={`/story/${collection.canonicalSlug}`}
-            publishDate={collection.publishDate}
-            headingLevel={2}
-            description=<Body
-              nodeData={truncateAmat(JSON.parse(collection.description))}
-            />
-            image={
-              collection.primaryVisuals?.lead ? (
-                <Image
-                  image={collection.primaryVisuals.lead}
-                  aspectRatio="uncropped"
-                  sizes="(max-width: 590px) 95vw, (max-width: 890px) 45vw, 300px"
-                  alt={collection.primaryVisuals.lead.longCaption}
-                />
-              ) : (
-                <Image fallbackSrc={fallback} alt="" />
-              )
-            }
+          key={collection.id}
+          id={collection.id}
+          title={collection.title}
+          href={`/story/${collection.canonicalSlug}`}
+          publishDate={collection.publishDate}
+          headingLevel={2}
+          image={
+            collection.primaryVisuals?.lead ? (
+              <Image
+                image={collection.primaryVisuals.lead}
+                aspectRatio="uncropped"
+                sizes="(max-width: 590px) 95vw, (max-width: 890px) 45vw, 300px"
+                alt={collection.primaryVisuals.lead.longCaption}
+              />
+            ) : (
+              <Image fallbackSrc={fallback} alt="" />
+            )
+          }
+          description=<Body
+            nodeData={truncateAmat(JSON.parse(collection.description))}
+          />
           />
         );
       })}
-    </div>
+    </section>
   );
 };
+
 Collection.propTypes = {
   data: PropTypes.object
 };
+
 export default Collection;
