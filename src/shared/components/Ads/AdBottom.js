668c3b54e38598d3e02f288a898fef3e9dafbf37
Andrea Edstrom <aedstrom@mpr.org>
2019-05-24 14:02:12 -0500

    update ad text

--- src/shared/components/Ads/AdBottom.js
+++ src/shared/components/Ads/AdBottom.js
@@ -3,9 +3,8 @@ import React from 'react';
 const AdBottom = () => {
   return (
     <div id="mpr-mr-ads" className="adWrapper">
-      <div id="mpr-ad-2" className="ad ad_mr">
-        Medium Rectangle Ad 2
-      </div>
+      <span>Sponsor</span>
+      <div id="mpr-ad-2" className="ad ad_mr" />
     </div>
   );
 };
--- src/shared/components/Ads/AdTop.js
+++ src/shared/components/Ads/AdTop.js
@@ -3,9 +3,8 @@ import React from 'react';
 const AdTop = () => {
   return (
     <div id="mpr-mr-ads" className="adWrapper">
-      <div id="mpr-ad-1" className="ad ad_mr">
-        Medium Rectangle Ad 1
-      </div>
+      <span>Sponsor</span>
+      <div id="mpr-ad-1" className="ad ad_mr" />
     </div>
   );
 };
