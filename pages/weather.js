a47d6e6fdb9901e1cbd9dfcd11e9a1c81e901709
Kim Thompson <kthompson@mpr.org>
2019-06-06 11:26:48 -0500

    Moved the Weather state to the weather page

--- pages/weather.js
+++ pages/weather.js
@@ -1,11 +1,64 @@
+/* eslint react/display-name: 0 */
 import React from 'react';
 import Main from '../layouts/Main';
 import Weather from '../endpoints/Weather';
+import WeatherContext from '../endpoints/Weather/WeatherContext';
+import axios from 'axios';
+import Router from 'next/router';
 
-/* eslint react/display-name: 0 */
+class WeatherPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default () => (
-  <Main>
-    <Weather />
-  </Main>
-);
+    this.state = {
+      selectLocation: null,
+      weather: { response: {}, isLoaded: false, error: null },
+      handleOnChange: this.handleOnChange.bind(this)
+    };
+  }
+
+  componentDidMount() {
+    this.fetchWeatherData();
+  }
+
+  fetchWeatherData() {
+    const pathname = window.location.pathname.split('/');
+    const geoLocation = pathname[pathname.length - 1];
+    const coordinates = geoLocation.match(/-?\d+(\.\d+)?,\s*(-?\d+(\.\d+)?)/g)
+      ? geoLocation
+      : `44.9434,-93.0965`;
+
+    let url = `https://api.weather.gov/points/${coordinates}/forecast`;
+    axios
+      .get(url)
+      .then((res) => {
+        this.setState({
+          weather: { response: res.data, isLoaded: true }
+        });
+      })
+      .catch((error) => {
+        this.setState({ weather: { isLoaded: true, error: error } });
+      });
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      selectLocation: `${event.target.value}`
+    });
+    Router.push(`/weather/${event.target.value}`);
+
+    return this.fetchWeatherData();
+  }
+
+  render() {
+    return (
+      <Main>
+        <WeatherContext.Provider value={this.state}>
+          <Weather />
+        </WeatherContext.Provider>
+      </Main>
+    );
+  }
+}
+
+export default WeatherPage;
