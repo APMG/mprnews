ca706c14517d77cd3941bbf11476c02a87f487b2
Kim Thompson <kthompson@mpr.org>
2019-06-12 14:34:56 -0500

    I'm very close and I haven't committed all day oh no

--- endpoints/Weather/Weather.js
+++ endpoints/Weather/Weather.js
@@ -1,15 +1,78 @@
 import React, { useState, useEffect } from 'react';
+import { format, closestIndexTo } from 'date-fns';
 import PropTypes from 'prop-types';
 import { Heading } from '@apmg/titan';
+import { Image } from 'apm-mimas';
 import { weatherConfig } from '../../utils/defaultData';
+import { CtoF, degToCompass, mpsToMph, torrToInhg } from '../../utils/utils';
 
 const Weather = (props) => {
   // console.log(weather);
-  const [forecast, setForecast] = useState(props.forecast);
   const [location, setLocation] = useState(props.location);
+  const [weather, setWeather] = useState(props.weather);
+  const [forecast, setForecast] = useState(props.forecast);
   const [alerts, setAlerts] = useState(props.alerts);
+  let currentTime = Date.parse(weather.updateTime);
 
-  // console.log(alerts);
+  const getValueOfMostRecent = (arr) => {
+    let i = closestIndexTo(
+      currentTime,
+      arr.map((i) => Date.parse(i.validTime.split('/').shift()))
+    );
+    return arr[i].value;
+  };
+
+  const handleChange = async (e) => {
+    let newLocation = weatherConfig.find(
+      (item) => item.name === e.target.value
+    );
+    setLocation(newLocation);
+    const backgroundUrl = `https://api.weather.gov/points/${newLocation.lat},${
+      newLocation.long
+    }`;
+    const locationRes = await fetch(backgroundUrl);
+    const locationData = await locationRes.json();
+
+    const weatherUrl = locationData.properties.forecastGridData;
+    const weatherRes = await fetch(weatherUrl);
+    const weatherData = await weatherRes.json();
+
+    const forecastUrl = locationData.properties.forecast;
+    const forecastRes = await fetch(forecastUrl);
+    const forecastData = await forecastRes.json();
+
+    const alertUrl = `https://api.weather.gov/alerts/active?point=${
+      newLocation.lat
+    },${newLocation.long}`;
+    const alertRes = await fetch(alertUrl);
+    const alertData = await alertRes.json();
+
+    let {
+      updateTime,
+      temperature,
+      apparentTemperature,
+      windDirection,
+      windSpeed,
+      pressure,
+      dewpoint,
+      relativeHumidity
+    } = weatherData.properties;
+
+    setWeather({
+      updateTime,
+      temperature,
+      apparentTemperature,
+      windDirection,
+      windSpeed,
+      pressure,
+      dewpoint,
+      relativeHumidity
+    });
+    setForecast(forecastData.properties);
+    setAlerts(alertData.features);
+
+    console.log('weather', weather);
+  };
 
   return (
     <section className="weather">
@@ -17,14 +80,9 @@ const Weather = (props) => {
       <div className="weather_location">
         <Heading level={1}>{location.name}</Heading>
 
-        <select className="weather_locationSelect">
+        <select className="weather_locationSelect" onChange={handleChange}>
           {weatherConfig.map((loc) => (
-            <option
-              id={loc.id}
-              label={loc.name}
-              key={loc.id}
-              value={`${loc.lat},${loc.long}`}
-            >
+            <option key={loc.id} value={`${loc.name}`}>
               {loc.name}
             </option>
           ))}
@@ -32,8 +90,7 @@ const Weather = (props) => {
       </div>
 
       {alerts.map((alert) => {
-        console.log(alert);
-        // TODO: This should be a link, but i don't know how they're generating their link to the more detailed weather page on the current site from the data on this endpoint.
+        // This should be a link, but I can't figure out how to link to an endpoint for this alert given this data. However, we do have the raw alert data and could simply set this to expand and show the detailed description for ourselves. I think that's the best approach.
         return (
           <div key={alert.id} className="weather_alert">
             <Heading level={2}>{alert.properties.event}</Heading>
@@ -42,58 +99,64 @@ const Weather = (props) => {
       })}
 
       <div className="weather_share">
-        <button>Twitter</button>
-        <button>Facebook</button>
+        <button>Share via Twitter</button>
+        <button>Share via Facebook</button>
       </div>
 
       <div className="weather_current">
         <div className="weather_currentHeader">
           <Heading level={3}>Current Conditions</Heading>
-          <div>Time of last update</div>
+          <div>{format(weather.updateTime, 'h:mm A	MMM D, YYYY')}</div>
         </div>
         <div className="weather_currentTemp">
-          <div>77°</div>
-          <div>Feels like 77°</div>
+          <div>{`${CtoF(
+            getValueOfMostRecent(weather.temperature.values)
+          )}°`}</div>
+          <div>{`Feels like ${CtoF(
+            getValueOfMostRecent(weather.apparentTemperature.values)
+          )}°`}</div>
         </div>
         <div className="weather_currentIcon">
-          <div>*cloud icon*</div>
-          <div>Partly cloudy</div>
+          {/* This one (thank goodness) automatically sorts by time and puts the current one first */}
+          <Image
+            fallbackSrc={forecast.periods[0].icon}
+            alt={forecast.periods[0].shortForecast}
+          />
+          <div>{forecast.periods[0].shortForecast}</div>
         </div>
         <div className="weather_currentStats">
           <table>
             <tbody>
               <tr>
                 <td>Wind</td>
-                <td>WNW 10 mph</td>
+                <td>{`${degToCompass(
+                  getValueOfMostRecent(weather.windDirection.values)
+                )} ${mpsToMph(
+                  getValueOfMostRecent(weather.windSpeed.values)
+                )} mph`}</td>
               </tr>
               <tr>
                 <td>Pressure</td>
-                <td>↓ 30.11 inHg</td>
+                <td>{`${torrToInhg(
+                  getValueOfMostRecent(weather.pressure.values)
+                )}`}</td>
               </tr>
               <tr>
                 <td>Dew point</td>
-                <td>38° F</td>
+                <td>{`${CtoF(
+                  getValueOfMostRecent(weather.temperature.values)
+                )}° F`}</td>
               </tr>
               <tr>
                 <td>Humidity</td>
-                <td>24%</td>
+                <td>{`${getValueOfMostRecent(
+                  weather.relativeHumidity.values
+                )}`}</td>
               </tr>
-              <tr>
-                <td>Sunrise</td>
-                <td>5:28 AM</td>
-              </tr>
-              <tr>
-                <td>Sunset</td>
-                <td>8:59 PM</td>
-              </tr>
+              {/* We used to provide sunset and sunrise, but weather.gov does not provide this. */}
             </tbody>
           </table>
         </div>
-      </div>
-
-      <div className="weather_today">
-        {/* TODO: insert a better visual for the hour-by-hour weather for today */}
-        {/* TODO: temperature chart */}
       </div>
 
       <div className="weather_forecast">
@@ -118,6 +181,7 @@ const Weather = (props) => {
 
 Weather.propTypes = {
   location: PropTypes.object,
+  weather: PropTypes.object,
   forecast: PropTypes.object,
   alerts: PropTypes.array
 };
--- pages/weather.js
+++ pages/weather.js
@@ -8,46 +8,77 @@ import { weatherConfig } from '../utils/defaultData';
 
 const defaultLocation = weatherConfig[0];
 
-const WeatherPage = ({ forecast, alerts }) => {
-  return (
-    <MainLayout>
-      <div>
-        <section className="stories section">
-          <Heading level={2}>Weather</Heading>
-          <Weather
-            location={defaultLocation}
-            forecast={forecast}
-            alerts={alerts}
-          />
-        </section>
-      </div>
-    </MainLayout>
-  );
-};
+const WeatherPage = ({
+  weather: {
+    updateTime,
+    temperature,
+    apparentTemperature,
+    windDirection,
+    windSpeed,
+    pressure,
+    dewpoint,
+    relativeHumidity
+  },
+  forecast,
+  alerts
+}) => (
+  <MainLayout>
+    <div>
+      <section className="stories section">
+        <Heading level={2}>Weather</Heading>
+        <Weather
+          location={defaultLocation}
+          weather={{
+            updateTime,
+            temperature,
+            apparentTemperature,
+            windDirection,
+            windSpeed,
+            pressure,
+            dewpoint,
+            relativeHumidity
+          }}
+          forecast={forecast}
+          alerts={alerts}
+        />
+      </section>
+    </div>
+  </MainLayout>
+);
 
 // getInitialProps can only be used in the /pages directory in Next.js
 WeatherPage.getInitialProps = async () => {
-  const forecastRes = await fetch(
+  const locationRes = await fetch(
     `https://api.weather.gov/points/${defaultLocation.lat},${
       defaultLocation.long
-    }/forecast`
-    // https://api.weather.gov/points/44.8848,-93.2223/forecast
+    }`
   );
+  const locationData = await locationRes.json();
+  const weatherUrl = locationData.properties.forecastGridData;
+  const forecastUrl = locationData.properties.forecast;
+
+  const weatherRes = await fetch(weatherUrl);
+  const weatherData = await weatherRes.json();
+
+  const forecastRes = await fetch(forecastUrl);
   const forecastData = await forecastRes.json();
 
   const alertRes = await fetch(
     `https://api.weather.gov/alerts/active?point=${defaultLocation.lat},${
       defaultLocation.long
     }`
-    //https://api.weather.gov/alerts/active?point=44.8848,-93.2223
   );
   const alertData = await alertRes.json();
 
-  return { forecast: forecastData.properties, alerts: alertData.features };
-  // forecastData.properties should have most of the info we need, while alertData's most relevant information should be in features. We can back out if we somehow need the rest
+  return {
+    weather: weatherData.properties,
+    forecast: forecastData.properties,
+    alerts: alertData.features
+  };
 };
 
 WeatherPage.propTypes = {
+  weather: PropTypes.object,
   forecast: PropTypes.object,
   alerts: PropTypes.array
 };
