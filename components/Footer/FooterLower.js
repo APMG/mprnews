fa3ca48a93d79ba987f1ec288bfe98e161ca0c3f
Matt Aho <maho@mpr.org>
2019-06-13 15:05:08 -0500

    Styles for lower footer content

--- components/Footer/FooterLower.js
+++ components/Footer/FooterLower.js
@@ -6,27 +6,34 @@ const FooterLower = (props) => {
   return (
     <div className="footer_lower">
       <div className="container">
-        <div className="footer_logo">
-          <img src="/static/mpr-logo-footer.svg" alt="Minnesota Public Radio" />
+        <div className="footer_lowerContent">
+          <div className="footer_logo">
+            <img
+              src="/static/mpr-logo-footer.svg"
+              alt="Minnesota Public Radio"
+            />
+          </div>
+          <div className="footer_nav">
+            <ul className="footer_navList">
+              {props.nav.map((item) => (
+                <li key={item.href} className="footer_navItem">
+                  <Link href={item.href}>
+                    <a className="footer_navLink">{item.label}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="footer_nav">
-          <ul className="footer_navList">
-            {props.nav.map((item) => (
-              <li key={item.href} className="footer_navItem">
-                <Link href={item.href}>
-                  <a>{item.label}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className="footer_lowerSmall">
+          <small className="footer_copyright">
+            &copy; {new Date().getFullYear()} Minnesota Public Radio. All rights
+            reserved.
+          </small>{' '}
+          <small className="footer_disclaimer">
+            Weather data provided by the National Weather Service
+          </small>
         </div>
-        <small className="footer_copy">
-          &copy; {new Date().getFullYear()} Minnesota Public Radio. All rights
-          reserved.
-        </small>
-        <small className="footer_disclaimer">
-          Weather data provided by the National Weather Service
-        </small>
       </div>
     </div>
   );
