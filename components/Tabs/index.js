e73e7b8b3657a5515e9a9023070c241c7be61b4c
Geoff Hankerson <ghankerson@mor.org>
2019-06-14 13:05:59 -0500

    styling for tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,9 +7,9 @@ const Tabs = (props) => {
     <ul className="tabs">
       {props.links.map((lnk) => {
         return (
-          <li key={lnk.day}>
+          <li key={lnk.key}>
             <Link href={lnk.href}>
-              <a className={lnk.className}>${lnk.day}</a>
+              <a className={lnk.isActive ? 'active' : ''}>{lnk.key}</a>
             </Link>
           </li>
         );
@@ -22,8 +22,7 @@ Tabs.propTypes = {
   links: PropTypes.arrayOf(
     PropTypes.shape({
       day: PropTypes.string,
-      href: PropTypes.string,
-      className: PropTypes.string
+      href: PropTypes.string
     })
   )
 };
