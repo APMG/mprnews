e2c9010cbfe2705170e8e0502bff97c9b16637e2
Geoff Hankerson <ghankerson@mor.org>
2019-06-18 16:07:17 -0500

    make search component a hook

--- endpoints/Search/Search.js
+++ endpoints/Search/Search.js
@@ -1,52 +1,38 @@
 /*global google*/
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.searchBarStyle = {};
-    this.state = {
-      sticky: false,
-      heightOffset: 0
-    };
-  }
-
-  setSearchRef = (element) => {
-    this.searchRef = element;
+const Search = () => {
+  const setSearchRef = () => {
     if (window.__gcse && window.__gcse.callback) {
       window.__gcse.callback();
     }
   };
 
-  componentDidMount() {
-    const srchBx = document.getElementById('gsearch');
-    if (!srchBx || !srchBx.children.length > 0) {
-      this.myCallback = function() {
-        if (document.readyState == 'complete') {
-          // Document is ready when CSE element is initialized.
-          // Render an element with both search box and search results in div with id 'test'.
-          google.search.cse.element.render({
-            div: 'gsearch',
-            tag: 'search'
-          });
-        } else {
-          // Document is not ready yet, when CSE element is initialized.
-          google.setOnLoadCallback(function() {
-            // Render an element with both search box and search results in div with id 'gsearch'.
-            google.search.cse.element.render({
-              div: 'gsearch',
-              tag: 'search'
-            });
-          }, true);
-        }
-      };
+  const myCallback = () => {
+    if (document.readyState == 'complete') {
+      // Document is ready when CSE element is initialized.
+      // Render an element with both search box and search results in div with id 'test'.
+      google.search.cse.element.render({
+        div: 'gsearch',
+        tag: 'search'
+      });
+    } else {
+      // Document is not ready yet, when CSE element is initialized.
+      google.setOnLoadCallback(function() {
+        // Render an element with both search box and search results in div with id 'gsearch'.
+        google.search.cse.element.render({
+          div: 'gsearch',
+          tag: 'search'
+        });
+      }, true);
     }
-
+  };
+  useEffect(() => {
     // Insert it before the CSE code snippet so that cse.js can take the script
     // parameters, like parsetags, callbacks.
     window.__gcse = {
       parsetags: 'explicit',
-      callback: this.myCallback
+      callback: myCallback
     };
 
     (function() {
@@ -58,14 +44,12 @@ class Search extends Component {
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(gcse, s);
     })();
-  }
-  render() {
-    return (
-      <div>
-        <div id="gsearch" ref={this.setSearchRef} />
-      </div>
-    );
-  }
-}
+  });
+  return (
+    <div>
+      <div id="gsearch" ref={setSearchRef} />
+    </div>
+  );
+};
 
 export default Search;
