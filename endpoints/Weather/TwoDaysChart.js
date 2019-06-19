7c319b4f64bad849a5feb3bfd3187fc0bb99a5c6
Kim Thompson <kthompson@mpr.org>
2019-06-19 13:16:00 -0500

    basic chart coming together"

--- endpoints/Weather/TwoDaysChart.js
+++ endpoints/Weather/TwoDaysChart.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { scaleLinear } from 'd3-scale';
+import { format } from 'date-fns';
 import WeatherIcon from '../../components/WeatherIcons/WeatherIcon';
 
-const BUFFER = 10;
+const BUFFER_BOTTOM = 10;
+const BUFFER_TOP = 25;
+const CHART_HEIGHT = 150;
+const CHART_WIDTH = 800;
 
 const TwoDaysChart = ({ forecast }) => {
   forecast.periods = forecast.periods.slice(0, 48);
@@ -12,38 +16,96 @@ const TwoDaysChart = ({ forecast }) => {
     forecast.periods.reduce(
       (min, b) => Math.min(min, b.temperature),
       forecast.periods[0].temperature
-    ) - BUFFER;
+    ) - BUFFER_BOTTOM;
 
   let hi =
     forecast.periods.reduce(
       (max, b) => Math.max(max, b.temperature),
       forecast.periods[0].temperature
-    ) + BUFFER;
+    ) + BUFFER_TOP;
 
   let tempScale = scaleLinear()
     .domain([lo, hi])
-    .range([0, 100]);
+    .range([0, CHART_HEIGHT]);
+
+  let colors = [
+    '#012942',
+    '#00607d',
+    '#009ea8',
+    '#32debd',
+    '#6fde85',
+    '#b0d646',
+    '#f4c300'
+  ];
+
+  console.log(forecast);
 
   return (
-    // I set this box to be 500 wide so that, for each of the 48 hours, I'd know I was just moving 10 pixels (with 10 px to spare on either side). Easy math :P. (actually, there's an attr "number" on each of these points that can literally be used to compute this. Just point.number * 10.)
-    <svg
-      viewBox="0 0 500 100"
-      className="chart"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {forecast.periods.map((period) => {
-        return (
-          <WeatherIcon
-            key={period.number}
-            width={10}
-            height={10}
-            x={`${period.number * 10}`}
-            y={`${tempScale(period.temperature)}`}
-            iconUrl={period.icon}
-          />
-        );
-      })}
-    </svg>
+    <div className="weather_chart">
+      <svg
+        className="weather_chartScrollable"
+        viewBox={`0 0 ${CHART_WIDTH} ${CHART_HEIGHT}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <line
+          x1={0}
+          y1={0}
+          x2={0}
+          y2={CHART_HEIGHT}
+          stroke="black"
+          strokeWidth={0.2}
+          opacity={0.4}
+        />
+        {forecast.periods.map((period, i) => {
+          let xPos = i * 25;
+          let yPos = tempScale(period.temperature);
+          let hour = format(
+            Date.parse(period.startTime.split('/').shift()),
+            'hA'
+          );
+
+          return (
+            <g key={period.number} className="weather_chartPoint">
+              <WeatherIcon
+                width={20}
+                height={20}
+                x={xPos + 3.5}
+                y={yPos}
+                iconUrl={period.icon}
+              />
+              <text x={`${xPos + 7.5}`} y={`${yPos + 30}`}>
+                {period.temperature}
+              </text>
+              <text
+                className="weather_chartPointHour"
+                x={`${xPos + 4.5}`}
+                y={CHART_HEIGHT - 4}
+              >
+                {hour}
+              </text>
+              <line
+                x1={xPos}
+                y1={0}
+                x2={xPos}
+                y2={CHART_HEIGHT}
+                stroke="black"
+                strokeWidth={0.2}
+                opacity={0.4}
+              />
+            </g>
+          );
+        })}
+        <line
+          x1={CHART_WIDTH}
+          y1={0}
+          x2={CHART_WIDTH}
+          y2={CHART_HEIGHT}
+          stroke="black"
+          strokeWidth={0.2}
+          opacity={0.4}
+        />
+      </svg>
+    </div>
   );
 };
 
