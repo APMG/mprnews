f7eb2f2be2ff6bf57dbec3a546621ccbbfd299ca
Kim Thompson <kthompson@mpr.org>
2019-06-20 14:48:09 -0500

    37991 two day graph 2

--- components/WeatherIcons/WeatherIconNightClear.js
+++ components/WeatherIcons/WeatherIconNightClear.js
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const WeatherIconNightClear = (props) => {
+const WeatherIconNightClear = ({ elementClass, x, y, width, height, fill }) => {
   const classes = classNames({
     weatherIcon: true,
     'weatherIcon_night-clear': true,
-    [props.elementClass]: props.elementClass
+    [elementClass]: elementClass
   });
 
   return (
     <svg
       className={classes}
+      width={width}
+      height={height}
+      x={x}
+      y={y}
+      fill={fill}
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 30 30"
@@ -32,7 +37,12 @@ const WeatherIconNightClear = (props) => {
 };
 
 WeatherIconNightClear.propTypes = {
-  elementClass: PropTypes.string
+  elementClass: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fill: PropTypes.string
 };
 
 export default WeatherIconNightClear;
--- components/WeatherIcons/WeatherIconTornado.js
+++ components/WeatherIcons/WeatherIconTornado.js
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const WeatherIconTornado = (props) => {
+const WeatherIconTornado = ({ elementClass, x, y, width, height, fill }) => {
   const classes = classNames({
     weatherIcon: true,
     weatherIcon_tornado: true,
-    [props.elementClass]: props.elementClass
+    [elementClass]: elementClass
   });
 
   return (
     <svg
       className={classes}
+      width={width}
+      height={height}
+      x={x}
+      y={y}
+      fill={fill}
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 30 30"
@@ -44,7 +49,12 @@ const WeatherIconTornado = (props) => {
 };
 
 WeatherIconTornado.propTypes = {
-  elementClass: PropTypes.string
+  elementClass: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fill: PropTypes.string
 };
 
 export default WeatherIconTornado;
