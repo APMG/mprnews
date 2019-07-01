230755f3facaf225b31969a9021425f06c131a09
Geoff Hankerson <ghankerson@mor.org>
2019-07-01 17:48:08 -0500

    Redmine #38253 QA feedback on feeds

--- server/feed.js
+++ server/feed.js
@@ -70,10 +70,11 @@ module.exports.feed = (server) => {
           'ddd, D MMM YYYY HH:mm:ss ZZ'
         );
         xml += `<item>
-                      <pubDate>${item.updatedAt}</pubDate>
+                      <pubDate>${dte}</pubDate>
                       <title>${item.title}</title>
-                      <description>${dte}</description>
+                      <description>${item.descriptionText}</description>
                       <link>https://www.mprnews.org${link}</link>
+                      <guid>https://www.mprnews.org${link}</guid>
                     </item>`;
       });
       xml += '</channel>';
