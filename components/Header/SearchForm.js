5bf76829b39cb6b326b69903c8cb0288850b7fcb
Geoff Hankerson <ghankerson@mor.org>
2019-07-01 18:21:54 -0500

    lint

--- components/Header/SearchForm.js
+++ components/Header/SearchForm.js
@@ -1,14 +1,8 @@
-import React, { useState } from 'react';
-import { Button } from '@apmg/titan';
+import React from 'react';
 import Icon from '../Icons/Icon';
 import Link from 'next/link';
 
 const SearchForm = () => {
-  const [value, setValue] = useState('');
-
-  const handleInputChange = (e) => {
-    setValue(e.target.value);
-  };
   return (
     <Link as={'search'} href={'/search'}>
       <a>
