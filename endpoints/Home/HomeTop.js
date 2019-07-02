dc837f671e94463f5aa72ba21840dec5445aa065
Matt Aho <maho@mpr.org>
2019-07-02 14:24:32 -0500

    Fix prettier

--- endpoints/Home/HomeTop.js
+++ endpoints/Home/HomeTop.js
@@ -7,9 +7,7 @@ const HomeTop = (props) => {
     <div>
       {props.info?.alert && props.info?.show_on?.indexOf('home') > -1 && (
         <Link href={props.info.url}>
-          <a className="alert-box">{`${props.info.prefix} ${
-            props.info.title
-          }`}</a>
+          <a className="alert-box">{`${props.info.prefix} ${props.info.title}`}</a>
         </Link>
       )}
     </div>
