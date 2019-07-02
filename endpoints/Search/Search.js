d70c2d1f0c6d2d8adc6486983ce049ff75844315
Geoff Hankerson <ghankerson@mor.org>
2019-07-02 14:38:31 -0500

    use useEffect more better to prevent re-render

--- endpoints/Search/Search.js
+++ endpoints/Search/Search.js
@@ -2,35 +2,26 @@
 import React, { useEffect } from 'react';
 
 const Search = () => {
-  const setSearchRef = () => {
-    if (window.__gcse && window.__gcse.callback) {
-      window.__gcse.callback();
-    }
-  };
-
   const myCallback = () => {
-    const gsearch = document.getElementById('gsearch');
-    if (gsearch.children.length === 0) {
-      if (document.readyState == 'complete') {
-        // Document is ready when CSE element is initialized.
-        // Render an element with both search box and search results in div with id 'test'.
+    if (document.readyState == 'complete') {
+      // Document is ready when CSE element is initialized.
+      // Render an element with both search box and search results in div with id 'test'.
+      google.search.cse.element.render({
+        div: 'gsearch',
+        tag: 'search'
+      });
+    } else {
+      // Document is not ready yet, when CSE element is initialized.
+      if (typeof google === 'undefined') {
+        return;
+      }
+      google.setOnLoadCallback(function() {
+        // Render an element with both search box and search results in div with id 'gsearch'.
         google.search.cse.element.render({
           div: 'gsearch',
           tag: 'search'
         });
-      } else {
-        // Document is not ready yet, when CSE element is initialized.
-        if (typeof google === 'undefined') {
-          return;
-        }
-        google.setOnLoadCallback(function() {
-          // Render an element with both search box and search results in div with id 'gsearch'.
-          google.search.cse.element.render({
-            div: 'gsearch',
-            tag: 'search'
-          });
-        }, true);
-      }
+      }, true);
     }
   };
   useEffect(() => {
@@ -41,19 +32,17 @@ const Search = () => {
       callback: myCallback
     };
 
-    (function() {
-      var cx = '009362219672105805737:m-ja7wfb1tc'; // Insert your own Custom Search engine ID here
-      var gcse = document.createElement('script');
-      gcse.type = 'text/javascript';
-      gcse.async = true;
-      gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(gcse, s);
-    })();
-  });
+    var cx = '009362219672105805737:m-ja7wfb1tc'; // Insert your own Custom Search engine ID here
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  }, []);
   return (
     <div>
-      <div id="gsearch" ref={setSearchRef} />
+      <div id="gsearch" />
     </div>
   );
 };
