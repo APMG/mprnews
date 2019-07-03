86cc7fdde97e410749a6dd718b68dd0866025a16
Andrea Edstrom <aedstrom@mpr.org>
2019-07-03 16:26:57 -0500

    update layout

--- layouts/ListenLayout.js
+++ layouts/ListenLayout.js
@@ -1,52 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/index.scss';
-import Link from 'next/link';
-import { Button, Heading } from '@apmg/titan';
-import FooterSubscribe from '../components/Footer/FooterSubscribe';
+import ListenFooter from '../components/ListenFooter/ListenFooter';
+import AudioPlayer from '../components/AudioPlayer/index';
 
 const ListenLayout = ({ children }) => (
   <>
+    <AudioPlayer />
     <main className="main main-listen">{children}</main>
-    <div className="playerFooter">
-      <div className="playerFooter_column">
-        <div className="playerFooter_row">
-          <Heading level={3} className="hdg hdg-5">
-            Member supported
-          </Heading>
-        </div>
-        <Button
-          href="https://contribute.publicradio.org/contribute.php?refId=default&WT.mc_id=news_web_nav_button&WT.mc_ev=click&utm_campaign=membership_contribution&utm_medium=web_nav_button&utm_source=news&utm_content=&utm_terms"
-          type="primary"
-        >
-          Become a Member
-        </Button>
-      </div>
-      <div className="playerFooter_column">
-        <div className="playerFooter_row">
-          <FooterSubscribe />
-        </div>
-      </div>
-      <div className="playerFooter_column">
-        <div className="playerFooter_row">
-          <Heading level={3} className="hdg hdg-5">
-            Where to listen
-          </Heading>
-        </div>
-        <ul className="vList">
-          <li>
-            <Link href="/schedule">
-              <a className="link link-plain">Program Schedule</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://www.mpr.org/listen/stations">
-              <a className="link link-plain">Station Directory</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <ListenFooter />
   </>
 );
 
--- pages/listen.js
+++ pages/listen.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Heading } from '@apmg/titan';
-import ListenLayout from '../layouts/ListenLayout';
 
 const ListenPage = () => {
   return (
-    <ListenLayout>
-      <div className="playerWrapper playerWrapper-listenpage js-player">
-        <div className="player player-listenpage">
-          <div className="player_wrapper player_wrapper-listenpage">
-            <div className="listen listen-highlight ">
-              <Heading level={2}>Now Listening to Live Stream</Heading>
-              <img
-                className="listen_thumbnail"
-                src="/static/MPRnews-default-audio-thumbnail.jpg"
-                alt="Minnesota Public Radio Audio Stream Thumbnail"
-              />
-            </div>
+    <div className="playerWrapper playerWrapper-listenpage js-player">
+      <div className="player player-listenpage">
+        <div className="player_wrapper player_wrapper-listenpage">
+          <div className="listen listen-highlight ">
+            <Heading level={2}>Now Listening to Live Stream</Heading>
+            <img
+              className="listen_thumbnail"
+              src="/static/MPRnews-default-audio-thumbnail.jpg"
+              alt="Minnesota Public Radio Audio Stream Thumbnail"
+            />
           </div>
         </div>
       </div>
-    </ListenLayout>
+    </div>
   );
+};
+
+ListenPage.getInitialProps = async () => {
+  return { layout: 'listen' };
 };
 
 export default ListenPage;
