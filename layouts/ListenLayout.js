1d5fbdfb63bd57b587656e18f7c0e3dfc34835ba
Andrea Edstrom <aedstrom@mpr.org>
2019-07-05 10:30:56 -0500

    add-header-to-listen-page

--- layouts/ListenLayout.js
+++ layouts/ListenLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/index.scss';
+import ListenHeader from '../components/ListenHeader/ListenHeader';
 import ListenFooter from '../components/ListenFooter/ListenFooter';
 import AudioPlayer from '../components/AudioPlayer/index';
 
 const ListenLayout = ({ children }) => (
   <div className="listenLayout">
-    <main className="main">
+    <ListenHeader />
+    <main className="main main-listen">
       <AudioPlayer />
       {children}
     </main>
