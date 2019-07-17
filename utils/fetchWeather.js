4ea85c55ce1ffe30897499006d14be5e9deb2f98
Kim Thompson <kthompson@mpr.org>
2019-07-17 13:47:17 -0500

    removed console log

--- utils/fetchWeather.js
+++ utils/fetchWeather.js
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-unfetch';
 
 export const fetchWeather = async (lat, long) => {
-  console.log(lat, long);
   try {
     const backgroundUrl = `https://api.weather.gov/points/${lat},${long}`;
     const locationRes = await fetch(backgroundUrl);
