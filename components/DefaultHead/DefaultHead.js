b81ea05ab0171219d8d5b0b8ce9cb0528f53ce9f
Kim Thompson <kthompson@mpr.org>
2019-08-12 12:00:20 -0500

    I had to move the tag manager into _app.js.
    It was loading multiple times because <DefaultHead /> was loaded
    multiple times (every time the Layout component was used). However,
    `dangerouslySetInnerHtml` cannot be used on the server side without the
    console yelling at you. Therefore, I had to use the plugin that looked
    like it had the best support.

--- components/DefaultHead/DefaultHead.js
+++ components/DefaultHead/DefaultHead.js
@@ -9,16 +9,6 @@ import appletouch from '../../static/apple-touch-icon.png';
 const DefaultHead = () => {
   const defaultTitle = globals.siteName;
 
-  const gtmId = 'GTM-KTT2Z2';
-  const gtmScriptHtml = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
-      {'gtm.start': new Date().getTime(),event:'gtm.js'}
-    );
-    var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-    j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','${gtmId}');`;
-  const createScript = () => {
-    return { __html: gtmScriptHtml };
-  };
-
   return (
     <Head>
       <meta charSet="utf-8" key="charset" />
@@ -84,7 +74,6 @@ const DefaultHead = () => {
         href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i|Roboto+Condensed:700|Roboto:400,700&display=swap&subset=latin-ext"
         rel="stylesheet"
       />
-      <script dangerouslySetInnerHTML={createScript()}></script>
     </Head>
   );
 };
