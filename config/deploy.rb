008a6c1fe036f488d7365090fc009e2dff68d21d
Geoff Hankerson <ghankerson@mor.org>
2019-11-25 13:16:45 -0600

    prettier
    Former-commit-id: bb4db6cda8e6d0c86b203610485e0baba3c844e3 [formerly bb4db6cda8e6d0c86b203610485e0baba3c844e3 [formerly 8d3ebf97b4b7e57e00e6a0abc8bed6d77d33dcc8]]
    Former-commit-id: 40f12f2c915af7988a09969b2dcba087afa1eb71
    Former-commit-id: 0d79d556a32fe5a696bce0e18e59898f08025cf5

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,6 @@ namespace :npm do
       within release_path do
         execute :npm, 'cache clean --force'
         execute :rm, '-rf ./node_modules'
-        execute :rm, 'package-lock.json'
         execute :npm, :install
         execute :npm, :run, :build
         execute :ln, '-s build _next'
