92084c2e0f190ce0e8514a7e3206f958cbda456c
Philip Kooistra <pkooistra@mpr.org>
2020-01-15 13:30:24 -0600

    fixing schedule
    Former-commit-id: 9af11bc4ab2ccca113edceedbaf3dc8f831e66fc

--- endpoints/Schedule/ScheduleInner.js
+++ endpoints/Schedule/ScheduleInner.js
@@ -24,6 +24,29 @@ const parseUrl = (url, typeUrl) => {
 };
 
 const ScheduleInner = ({ schedule }) => {
+  const getLink = (show) => {
+    const link = show.link ? show.link : show.external_link;
+    if (link) {
+      const urlHref = linkType(link, 'href');
+      const urlAs = linkType(link, 'as');
+      return (
+        <strong>
+          {link && (
+            <Link
+              href={`${urlHref}`}
+              as={`${urlAs}`}
+              className="link link-plain"
+            >
+              {show.name}
+            </Link>
+          )}
+        </strong>
+      );
+    } else {
+      return <strong>{show.name}</strong>;
+    }
+  };
+
   return (
     <ContentGrid>
       <table className="schedule">
@@ -35,22 +58,9 @@ const ScheduleInner = ({ schedule }) => {
                   <Time dateTime={program.start_dtim} formatString="h:mm aa" />
                 </td>
                 {program?.shows.map((show) => {
-                  let link = show.link ? show.link : show.external_link;
-                  let urlHref = linkType(link, 'href');
-                  let urlAs = linkType(link, 'as');
                   return (
                     <td key={show.id} className="schedule_rightmost">
-                      <strong>
-                        {link && (
-                          <Link
-                            href={`${urlHref}`}
-                            as={`${urlAs}`}
-                            className="link link-plain"
-                          >
-                            {show.name}
-                          </Link>
-                        )}
-                      </strong>{' '}
+                      {getLink(show)}{' '}
                       {program?.people.length > 0 && (
                         <>
                           <span>with </span>
