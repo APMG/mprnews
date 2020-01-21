932274b249d03950117c77eaa3cd21bddf1d49ce
Geoff Hankerson <ghankerson@mor.org>
2020-01-21 13:27:38 -0600

    Redmine #41175 amp stories
    Former-commit-id: 4fc053e3a5040adc45238c7bb6bce34d0a3b8e44

--- pages/story/[...slug].js
+++ pages/story/[...slug].js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ErrorPage from 'next/error';
-import Story from '../endpoints/Story/Story';
-import ContentGrid from '../grids/ContentGrid';
-import Sidebar from '../components/Sidebar/Sidebar';
-import initApollo from '../lib/init-apollo';
-import query from '../endpoints/Story/story.gql';
+import Story from '../../endpoints/Story/Story';
+import ContentGrid from '../../grids/ContentGrid';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import initApollo from '../../lib/init-apollo';
+import query from '../../endpoints/Story/story.gql';
 import {
   fetchMemberDriveStatus,
   addMemberDriveElements
-} from '../utils/membershipUtils';
+} from '../../utils/membershipUtils';
 
 const StoryPage = ({ data, errorCode }) => {
   if (errorCode) return <ErrorPage statusCode={errorCode} />;
@@ -43,26 +43,26 @@ StoryPage.getInitialProps = async ({
     query: query,
     variables: {
       contentAreaSlug: process.env.CONTENT_AREA_SLUG,
-      slug: slug,
+      slug: slug.join('/'),
       previewToken: previewToken
     }
   })
     .then((result) => {
       data = result.data;
-      if (res && !data.story) {
-        res.status(404);
+      if (!data.story) {
+        res.statusCode = 404;
         errorCode = res.statusCode > 200 ? res.statusCode : false;
       }
       if (
         res &&
         data?.story?.canonicalSlug &&
-        data.story.canonicalSlug !== slug
+        data.story.canonicalSlug !== slug.join('/')
       ) {
-        res.redirect(`/story/${data.story.canonicalSlug}`);
+        res.writeHead(301, { Location: `/story/${data.story.canonicalSlug}` });
       }
     })
     .catch(() => {
-      res.status(404);
+      res.statusCode = 404;
       errorCode = res.statusCode > 200 ? res.statusCode : false;
     });
 
