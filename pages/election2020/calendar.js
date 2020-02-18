33d3706d348f9fe8c9b03fb6ca3c65bda2f011d7
Kooistra, Philip <pkooistra@mpr.org>
2020-02-18 12:35:50 -0600

    Fixing election2020 calendar endpoint
    Former-commit-id: 9db3ae59d7f72078a86932a1f9de7c20c8869384

--- pages/election2020/calendar.js
+++ pages/election2020/calendar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
-import Calendar from '../endpoints/Calendar/Calendar';
+import Calendar from '../../endpoints/Calendar/Calendar';
 import absoluteUrl from 'next-absolute-url';
 
 const CalendarPage = ({ events }) => {
