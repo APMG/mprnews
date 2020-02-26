1d3d05054491387fa3ec70e6d49706ab3e736d1c
Andrea Edstrom <aedstrom@mpr.org>
2020-02-26 10:22:11 -0600

    update so the collection title is added a class
    Former-commit-id: 287968400441bbd3815df08090560a38581de318

--- endpoints/Collection/CollectionTwoColumn.js
+++ endpoints/Collection/CollectionTwoColumn.js
@@ -13,7 +13,9 @@ const Collection = ({ data: { collection, alertConfig } }) => {
   let alerts = alertConfig ? JSON.parse(alertConfig.json) : null;
   const contentTopicCollectionRef = useRef(null);
   let checkCollectionName = `${collection?.title}`;
-
+  let collectionClassName = checkCollectionName
+    .toLowerCase()
+    .replace(/\s/g, '');
   if (!checkCollectionName) {
     checkCollectionName = 'default';
   }
@@ -53,9 +55,7 @@ const Collection = ({ data: { collection, alertConfig } }) => {
         <div className="collection_header">
           <Heading
             level={1}
-            className={`hdg hdg-section ${
-              collection.title === 'Election 2020' ? 'hidden' : ''
-            }`}
+            className={`hdg hdg-section ${collectionClassName}`}
           >
             {collection?.title}
           </Heading>
