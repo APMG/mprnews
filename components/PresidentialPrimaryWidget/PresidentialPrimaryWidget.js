b9c4161a20b6912d769712ae5e8050b45d0294d8
Geoff Hankerson <ghankerson@mor.org>
2020-03-03 11:38:55 -0600

    Update election  link
    Former-commit-id: 5a5ccd57fd21877de590b3981cf7d71f0a7d0c45

--- components/PresidentialPrimaryWidget/PresidentialPrimaryWidget.js
+++ components/PresidentialPrimaryWidget/PresidentialPrimaryWidget.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PresidentialPrimaryResults from './PresidentialPrimaryResults';
 import PresidentialPrimaryDelegates from './PresidentialPrimaryDelegates';
-import { Link } from '@apmg/titan';
 import Icon from '../Icons/Icon';
 import PropTypes from 'prop-types';
 
@@ -29,10 +28,10 @@ const PresidentialPrimaryWidget = ({ states, showDelegateCount }) => {
       )}
       {states?.length && <PresidentialPrimaryResults states={states} />}
       <div className="resultsLink">
-        <Link href="[...slug]" as="/politics/election-2020">
+        <a href="https://elections.mpr.org/2020-03-03">
           View More Results
           <Icon name="chevronRight" />
-        </Link>
+        </a>
       </div>
     </div>
   );
