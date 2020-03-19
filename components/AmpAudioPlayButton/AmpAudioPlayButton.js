726e5e1439a2ba97d6e9255c7ffe673d2ab83346
Lilith Bentley <lbentley@mpr.org>
2020-03-19 10:14:18 -0500

    Style amp audio controls
    Former-commit-id: 099c9e37ea6cfab8e45c50b78414468fb79fde5b

--- components/AmpAudioPlayButton/AmpAudioPlayButton.js
+++ components/AmpAudioPlayButton/AmpAudioPlayButton.js
@@ -2,14 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ampStyles = {
-  figure: { margin: '0' }
+  figure: {
+    margin: '0 0 1em',
+    fontFamily: 'Roboto, system-ui, -apple-system, sans-serif',
+    fontSize: '.85em'
+  },
+  figcaption: {
+    padding: '0.25em 1em 0'
+  },
+  audio: {
+    height: '42px',
+    width: '100%',
+    minWidth: '300px'
+  }
 };
 
 const AmpAudioPlayButton = ({ audioSource, audioTitle, audioSubtitle }) => {
   return (
     <figure style={ampStyles.figure}>
-      <amp-audio width="300" height="42" src={audioSource}></amp-audio>
-      <figcaption className="figure_caption">
+      <amp-audio
+        height="42"
+        width="auto"
+        layout="fixed-height"
+        src={audioSource}
+      ></amp-audio>
+      <figcaption className="figure_caption" style={ampStyles.figcaption}>
         <div className="figure_caption_content">{audioTitle}</div>
         <span className="figure_credit">{audioSubtitle}</span>
       </figcaption>
