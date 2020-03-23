082560ef1c68b49a998cd7f4dba5c5ef43710cdd
Geoff Hankerson <ghankerson@mor.org>
2020-03-23 08:14:00 -0500

    Add charbeat analytics to amp pages
    Former-commit-id: d6e8466161ab51cc24b614576be5d1402d8a3940

--- layouts/AmpLayout.js
+++ layouts/AmpLayout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AmpHeader from '../components/AmpHeader/AmpHeader';
 import AmpFooter from '../components/AmpFooter/AmpFooter';
+import Head from 'next/head';
 
 const ampStyles = {
   ampContainer: {
@@ -9,15 +10,40 @@ const ampStyles = {
   }
 };
 
-const AmpLayout = ({ children }) => (
-  <>
-    <AmpHeader />
-    <main>
-      <div style={ampStyles.ampContainer}>{children}</div>
-    </main>
-    <AmpFooter />
-  </>
-);
+const chartbeatObj = {
+  vars: {
+    uid: '33583',
+    domain: 'mprnews.org'
+  }
+};
+
+const AmpLayout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <script
+          async
+          custom-element="amp-analytics"
+          src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"
+        ></script>
+      </Head>
+      <AmpHeader />
+      <main>
+        <amp-analytics type="chartbeat">
+          <script
+            type="application/json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(chartbeatObj)
+            }}
+          />
+        </amp-analytics>
+
+        <div style={ampStyles.ampContainer}>{children}</div>
+      </main>
+      <AmpFooter />
+    </>
+  );
+};
 
 AmpLayout.propTypes = {
   children: PropTypes.any
